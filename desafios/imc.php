880cef454cd4dcbf0846e3cdb79ba27fe5411158
Gabriel Teodoro <biel.teodorob@gmail.com>
2023-07-15 18:35:59 -0300

    first commit

---
+++ desafios/imc.php
@@ -0,0 +1,7 @@
+<?php
+
+$peso = 95;
+$altura = 1.68;
+$imc = $peso / ($altura * 2);
+
+echo $imc;
---
+++ desafios/tabuada.php
@@ -0,0 +1,8 @@
+<?php
+
+$multiplicador = 9;
+
+for($i = 0; $i <= 9; $i++){
+    echo "$i x $multiplicador = " . $i * $multiplicador . PHP_EOL;
+}
+
